4a8b68df19f822c0bdda18268d03ec80e12ea0d1
Rayan <rayanyassen23@gmail.com>
2023-11-19 10:42:48 +0200

    Initial commit

---
+++ app/src/main/java/kh/adam11/MainActivity.java
@@ -0,0 +1,17 @@
+package kh.adam11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv_Output
+            private TextView RAMGO
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
